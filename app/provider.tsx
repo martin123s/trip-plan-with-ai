26e6e395f70fc2765579089470ab59745b85e047
martin123s <martin.xzf@gmail.com>
2025-09-11 14:00:24 -0400

    init

---
+++ app/provider.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Provider = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Provider
